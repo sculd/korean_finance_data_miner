360a0f1ff488ee028a8a2f0ae1dedc8855fb0b8c
Jun Lim <hjunlim@google.com>
2019-09-25 07:43:25 -0700

    add config, add schedule

--- run.py
+++ run.py
@@ -1,10 +1,21 @@
+import datetime, time
+import config
 import ingest.ingest
 from ingest import combine_ingest
 
-
 def run():
-	#ingest.ingest.run()
+	ingest.ingest.run()
 	combine_ingest.run()
 
 if __name__ == '__main__':
-	run()
+	cfg = config.load('config.kr.yaml')
+
+	run_dates = set()
+	while True:
+		dt_str = str(datetime.datetime.utcnow().astimezone(config.get_tz(cfg)).date())
+		if dt_str in run_dates:
+			time.sleep(10 * 60)
+			continue
+		run_dates.add(str(dt_str))
+		run()
+
