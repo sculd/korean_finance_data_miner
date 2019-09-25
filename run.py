2d0e1154044d2acebbd96e30e33c151b64e887ae
Jun Lim <hjunlim@google.com>
2019-09-25 09:42:15 -0700

    print->logging.info in run

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import util.time
 import config
 import ingest.ingest
 from ingest import combine_ingest
+import logging
 
 def run():
 	ingest.ingest.run()
@@ -15,19 +16,19 @@ if __name__ == '__main__':
 	while True:
 		tz = config.get_tz(cfg)
 		dt_str = str(util.time.get_utcnow().astimezone(tz).date())
-		print('checking if run for {dt_str} should be done'.format(dt_str=dt_str))
+		logging.info('checking if run for {dt_str} should be done'.format(dt_str=dt_str))
 		if dt_str in run_dates:
-			print('run for {dt_str} is already done'.format(dt_str=dt_str))
+			logging.info('run for {dt_str} is already done'.format(dt_str=dt_str))
 			time.sleep(10 * 60)
 			continue
 		t_run_after = config.get_start(cfg)
 		while True:
 			t_cur = util.time.get_utcnow().astimezone(tz).time()
-			print('checking if the schedule time for {dt_str} has reached'.format(dt_str=dt_str))
+			logging.info('checking if the schedule time for {dt_str} has reached'.format(dt_str=dt_str))
 			if t_cur > t_run_after:
 				run_dates.add(str(dt_str))
 				run()
 				break
-			print('schedule time {t_run_after} not yet reached at {t_cur}'.format(t_run_after=t_run_after, t_cur=t_cur))
+			logging.info('schedule time {t_run_after} not yet reached at {t_cur}'.format(t_run_after=t_run_after, t_cur=t_cur))
 			time.sleep(60)
 
