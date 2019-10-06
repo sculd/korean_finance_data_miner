f47cdfde2e54d51d624433261eb53a1f265537a7
Jun Lim <hjunlim@google.com>
2019-10-05 17:25:35 -0700

    2019-10-04

--- util/time.py
+++ util/time.py
@@ -12,6 +12,9 @@ def get_now_tz():
     return get_utcnow().astimezone(tz)
 
 def get_today_str_tz():
+    '''
+    :return: e.g: 2019-12-25
+    '''
     cfg = config.load('config.kr.yaml')
     tz = config.get_tz(cfg)
     return str(get_utcnow().astimezone(tz).date())
